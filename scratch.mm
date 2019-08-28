d380471c937ebe13a7ba5692bf67591d6576227c
Mark Young <mark.young@cuanswers.com>
2019-08-28 08:03:47 -0400

    Added screen shots, mind map, and updated scratch file

---
+++ scratch.mm
@@ -0,0 +1,101 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Scratch" FOLDED="false" ID="ID_576992921" CREATED="1566931548166" MODIFIED="1566931555520" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Show screen shots of scratch site" POSITION="right" ID="ID_1903703255" CREATED="1566931618396" MODIFIED="1566931639900">
+<edge COLOR="#ff0000"/>
+<node TEXT="Home" ID="ID_1818441082" CREATED="1566931640316" MODIFIED="1566931683246"/>
+<node TEXT="Explore" ID="ID_1328553020" CREATED="1566931683500" MODIFIED="1566931718055"/>
+<node TEXT="Tutorials" ID="ID_166043047" CREATED="1566931718444" MODIFIED="1566931721166"/>
+<node TEXT="Wiki - documentation" ID="ID_568274602" CREATED="1566933645972" MODIFIED="1566933652104"/>
+<node TEXT="Forum" ID="ID_1816678751" CREATED="1566933640613" MODIFIED="1566933644528"/>
+<node TEXT="Create" ID="ID_73662400" CREATED="1566931721349" MODIFIED="1566931733830"/>
+<node TEXT="My Stuff" ID="ID_101323277" CREATED="1566931733996" MODIFIED="1566931752150"/>
+<node TEXT="View Project" ID="ID_607564444" CREATED="1566931752317" MODIFIED="1566931760953">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node TEXT="Scratch history" POSITION="right" ID="ID_1159691434" CREATED="1566933566660" MODIFIED="1566933572120">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Looking at code" POSITION="right" ID="ID_461159624" CREATED="1566932743879" MODIFIED="1566932748928">
+<edge COLOR="#0000ff"/>
+<node TEXT="Simple scripts" ID="ID_328348242" CREATED="1566932749190" MODIFIED="1566932757487">
+<node TEXT="Walking bear logo" ID="ID_476530679" CREATED="1566932794791" MODIFIED="1566932850473">
+<node TEXT="Discuss code blocks" ID="ID_1899616971" CREATED="1566933728822" MODIFIED="1566933740375"/>
+</node>
+<node TEXT="Area sprite" ID="ID_337201903" CREATED="1566932850958" MODIFIED="1566932981098">
+<node TEXT="Flashes when receiving ChangeZone event" ID="ID_579571410" CREATED="1566932982816" MODIFIED="1566933006963"/>
+<node TEXT="Discuss costumes" ID="ID_62521814" CREATED="1566933824598" MODIFIED="1566933845288"/>
+<node TEXT="Discuss broadcast/receive events" ID="ID_673881403" CREATED="1566933982911" MODIFIED="1566933999498"/>
+</node>
+<node TEXT="Mountains - background" ID="ID_988078922" CREATED="1566933254594" MODIFIED="1566933263437">
+<node TEXT="Discuss variable types" ID="ID_322485243" CREATED="1566933966065" MODIFIED="1566934023364"/>
+<node TEXT="Discuss sprites as classes" ID="ID_1104781672" CREATED="1566934024047" MODIFIED="1566934033219"/>
+</node>
+</node>
+</node>
+</node>
+</map>
